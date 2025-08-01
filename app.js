b409dd18c817550737d61262c8752e37ddfec318
Daniel Nascimento <you@example.comgit config --global user.email you@example.com>
2025-08-01 00:42:22 -0300

    Implementada função com retorno para geração de número aleatório

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let numeroSecreto = gerarNumeroAleatorio ();
+
 function exibirTextoNaTela (tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto
@@ -6,6 +8,10 @@ function exibirTextoNaTela (tag, texto){
 exibirTextoNaTela('h1', 'Jogo do número sercreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
-function verificarChute() {
-    console.log('O botão foi clicado"')
+function verificarChute(){
+    console.log(numeroSecreto)
+}
+
+function gerarNumeroAleatorio (){
+    return parseInt(Math.random () * 10 + 1);
 }
