c860157afe9b23dcc1a23cc4fcf81d6b04448821
Daniel Nascimento <you@example.comgit config --global user.email you@example.com>
2025-08-05 18:05:44 -0300

    Adicionada a função de alterar os paragrafos e titulos conforme o chute e de verificar se o número secreto é maior ou menorque o digitado pelo usuário

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-let numeroSecreto = gerarNumeroAleatorio ();
+let numeroSecreto = gerarNumeroAleatorio();
 
-function exibirTextoNaTela (tag, texto){
+function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
-    campo.innerHTML = texto
+    campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número sercreto');
+exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto');
+    } else {
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor do que o chute');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior do que o chute');
+        }
+    }
 }
 
-function gerarNumeroAleatorio (){
-    return parseInt(Math.random () * 10 + 1);
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
 }
