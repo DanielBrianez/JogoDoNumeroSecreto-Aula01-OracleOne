356befb6936addd22136c5ae7820c205e5a616b7
Daniel Nascimento <you@example.comgit config --global user.email you@example.com>
2025-08-08 03:07:14 -0300

    Adicionada a função de lista, numero limite e limpeza de lista ao atingir o numero maximo de tentativas

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1 ;
+
  mensagemInicial();
 
 function exibirTextoNaTela(tag, texto){
@@ -9,7 +12,7 @@ function exibirTextoNaTela(tag, texto){
 
 function mensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}`);
 
 }
 
@@ -34,7 +37,19 @@ function verificarChute(){
 }
 
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista ==  numeroLimite){
+        listaDeNumerosSorteados = []
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
     
 }
 
